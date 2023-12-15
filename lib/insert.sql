f27a80a35a819ff2f7744cac93401aaf5b2ded04
evans <evansnt9619@gmail.com>
2023-12-15 13:03:45 +0300

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 2, "F", "Brown", "Playful", "False");
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 5, "F", "Black", "Calm", "True");
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 4, "M", "Brown", "Assertive", "True");
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 6, "F", "Golden", "Friendly", "False");
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 2, "M", "Green", "Mischievous", "True");
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 3, "F", "Gray", "Adventurous", "False");
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, "M", "Black", "Energetic", "True");
+
